e3c4f67f6cb83e702d82465ebd8a4bac7e946832
abdel-boudaoud <abdelkaderfarm@gmail.com>
2023-10-16 17:45:27 +0100

    Add PUSH to stack and a do nothing instruction

--- cpu.js
+++ cpu.js
@@ -7,6 +7,7 @@ function CPU() {
   // 4=> ACC (accumulator)
   let registers = [0, 0, 0, 0, 0];
   let regs = [`RA`, `RB`, `RC`, `RD`, `ACC`];
+  let stack = [];
   let halted = false;
   function run(memory) {
     for (let PC = 0; PC < memory.length; PC++) {
@@ -15,6 +16,7 @@ function CPU() {
       }
       switch (memory[PC]) {
         case 10:
+          //MOV
           PC++;
           if (parseInt([memory[PC + 1]], 10)) {
             registers[memory[PC]] = parseInt([memory[PC + 1]], 10);
@@ -26,6 +28,7 @@ function CPU() {
 
           break;
         case 20:
+          //ADD
           PC++;
           if (regs.includes(memory[PC + 1])) {
             registers[memory[PC]] += registers[regs.indexOf(memory[PC + 1])];
@@ -37,6 +40,7 @@ function CPU() {
           break;
 
         case 30:
+          //SUB
           PC++;
           if (regs.includes(memory[PC + 1])) {
             registers[memory[PC]] -= registers[regs.indexOf(memory[PC + 1])];
@@ -47,6 +51,7 @@ function CPU() {
           PC++;
           break;
         case 40:
+          //MUL
           PC++;
           if (regs.includes(memory[PC + 1])) {
             registers[memory[PC]] *= registers[regs.indexOf(memory[PC + 1])];
@@ -57,7 +62,9 @@ function CPU() {
           PC++;
           break;
         case 50:
+          //DIV
           PC++;
+
           if (regs.includes(memory[PC + 1])) {
             registers[memory[PC]] /= registers[regs.indexOf(memory[PC + 1])];
           } else {
@@ -66,23 +73,35 @@ function CPU() {
 
           PC++;
           break;
-          case 60:
-            PC++
-            PC += memory[PC]
-            PC++
-            break
-          case 90:
-            PC++
-            registers[memory[PC]] = parseInt([memory[PC + 1]], 10);
-            PC++
-            break
-            
+        case 60:
+          //JUMP
+          PC++;
+          PC += memory[PC];
+          PC++;
+          break;
+        case 90:
+          //LOAD
+          PC++;
+          registers[memory[PC]] = parseInt([memory[PC + 1]], 10);
+          PC++;
+          break;
+        case 110:
+          //PUSH TO STACK
+          PC++;
+          stack.push(memory[PC]);
+          PC++;
+          break;
+        case 160:
+          //DO NOTHING
+          PC++;
+          break;
       }
     }
   }
 
   function print() {
     console.log(registers);
+    console.log(`the stack => `, stack);
   }
   function load(instructions) {
     let memory = [...instructions];
@@ -96,4 +115,6 @@ function CPU() {
 
 let cpu = CPU();
 
-cpu.load([90, 1, 50]);
+cpu.load([
+  90, 1, 50, 10, 0, 80, 110, 50, 160, 10, 0, 50, 10, 0, 250, 110, 90, 160,
+]);
