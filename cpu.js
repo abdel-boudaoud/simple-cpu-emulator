194cf2a2477adf813eda51a5301c9779eed3f5df
abdel-boudaoud <abdelkaderfarm@gmail.com>
2023-10-16 14:59:53 +0100

    Add Division instruction

--- cpu.js
+++ cpu.js
@@ -1,88 +1,88 @@
-function CPU(){
-    //CPU REGISTERS
-    // 0=>RA
-    // 1=>RB
-    // 2=>RC
-    // 3=>RD
-    // 4=> ACC (accumulator)
-    let registers = [0,0,0,0,0]
-    let regs = [`RA`, `RB`, `RC`, `RD`, `ACC`]
-    let halted = false;
-    function run(memory){
-       
-        for(let PC = 0 ;PC < memory.length; PC++){
-            if(halted == true){
-                break
-            }
-            switch(memory[PC]){
-                case 10:
-                    PC++
-                    if(parseInt([memory[PC+1]], 10)){
-                        registers[memory[PC]]= parseInt([memory[PC+1]], 10)
-                    }else if(regs.includes(memory[PC+1])){
-                        registers[memory[PC]] =  registers[regs.indexOf(memory[PC+1])]
-                    }
-                         
-                   
-                    PC++
-                    
-                    break
-                case 20:
-                    
-                    PC++
-                    if(regs.includes(memory[PC+1])){
-                        registers[memory[PC]] +=  registers[regs.indexOf(memory[PC+1])]
-                    }else{
-                        registers[memory[PC]] += memory[PC+1]
-                    }
-                    
-                    PC++
-                    break
+function CPU() {
+  //CPU REGISTERS
+  // 0=>RA
+  // 1=>RB
+  // 2=>RC
+  // 3=>RD
+  // 4=> ACC (accumulator)
+  let registers = [0, 0, 0, 0, 0];
+  let regs = [`RA`, `RB`, `RC`, `RD`, `ACC`];
+  let halted = false;
+  function run(memory) {
+    for (let PC = 0; PC < memory.length; PC++) {
+      if (halted == true) {
+        break;
+      }
+      switch (memory[PC]) {
+        case 10:
+          PC++;
+          if (parseInt([memory[PC + 1]], 10)) {
+            registers[memory[PC]] = parseInt([memory[PC + 1]], 10);
+          } else if (regs.includes(memory[PC + 1])) {
+            registers[memory[PC]] = registers[regs.indexOf(memory[PC + 1])];
+          }
 
-                case 30:
-                    PC++
-                    if(regs.includes(memory[PC+1])){
-                        registers[memory[PC]] -=  registers[regs.indexOf(memory[PC+1])]
-                    }else{
-                        registers[memory[PC]] -= memory[PC+1]
-                    }
-                    
-                    PC++
-                    break
-                    case 40:
-                        PC++
-                        if(regs.includes(memory[PC+1])){
-                            registers[memory[PC]] *=  registers[regs.indexOf(memory[PC+1])]
-                        }else{
-                            registers[memory[PC]] *= memory[PC+1]
-                        }
-                        
-                        PC++
-                        break
-                    
-            }
-        }
-        
-       
-        
+          PC++;
+
+          break;
+        case 20:
+          PC++;
+          if (regs.includes(memory[PC + 1])) {
+            registers[memory[PC]] += registers[regs.indexOf(memory[PC + 1])];
+          } else {
+            registers[memory[PC]] += memory[PC + 1];
+          }
+
+          PC++;
+          break;
+
+        case 30:
+          PC++;
+          if (regs.includes(memory[PC + 1])) {
+            registers[memory[PC]] -= registers[regs.indexOf(memory[PC + 1])];
+          } else {
+            registers[memory[PC]] -= memory[PC + 1];
+          }
+
+          PC++;
+          break;
+        case 40:
+          PC++;
+          if (regs.includes(memory[PC + 1])) {
+            registers[memory[PC]] *= registers[regs.indexOf(memory[PC + 1])];
+          } else {
+            registers[memory[PC]] *= memory[PC + 1];
+          }
+
+          PC++;
+          break;
+        case 50:
+          PC++;
+          if (regs.includes(memory[PC + 1])) {
+            registers[memory[PC]] /= registers[regs.indexOf(memory[PC + 1])];
+          } else {
+            registers[memory[PC]] /= memory[PC + 1];
+          }
+
+          PC++;
+          break;
+      }
     }
+  }
 
-    function print(){
-        console.log(registers)
-    }
-    function load(instructions){
-        let memory = [...instructions]
+  function print() {
+    console.log(registers);
+  }
+  function load(instructions) {
+    let memory = [...instructions];
 
-        run(memory)
+    run(memory);
 
-        print()
-    }
-    return {run, print, load};
+    print();
+  }
+  return { run, print, load };
 }
-
 
 let cpu = CPU();
 
-cpu.load([10, 0, 5,10, 4, 100 , 40, 0, `ACC`])
-
-
+cpu.load([10, 0, 100, 10, 1, 2, 50, 0, `RB`, 10, 4, `RA`, 50, 4, `RB`]);
